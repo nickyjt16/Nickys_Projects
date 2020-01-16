ef7936f9eb61d1f834409f84253343c98730d1bc
Nicky <nickyjt16@gmail.com>
2020-01-16 23:18:35 +0000

    Front end created
    Front end is there, need to get the "list all notes" function displaying then finish the rest of the python for the backend

--- api/api.py
+++ api/api.py
@@ -1,10 +1,15 @@
 import flask
 from flask import request, jsonify
 import sqlite3
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+#helper function for returning all notes
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
@@ -12,38 +17,45 @@ def dict_factory(cursor, row):
     return d
 
 @app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home():
     return "<h1>Nicky's Coding Challenge to Claranet</h1><p>This is the homepage of my API site.</p>"
 
 #Create
-@app.route('/api/v1/resources/entries/create', methods=['POST'])
+@app.route('/api/v1/resources/notes/', methods=['POST'])
 def api_create():
-    #TODO
-    return null
+    conn = sqlite3.connect('notes.db')
+    c = conn.cursor()
+    newNote = request.args.get('newNote')
+    user = request.args.get('user')
+    params = (newNote, user)
+    conn.execute('INSERT INTO Notes (Note, User) VALUES (?, ?)', params)
+    conn.commit()
+    conn.close()
+    return "Your new note has been logged in the system"
 
 #Read (or list as specified in the brief)
-@app.route('/api/v1/resources/entries/all', methods=['GET'])
+@app.route('/api/v1/resources/notes/all', methods=['GET'])
+@app.route('/api/v1/resources/notes/', methods=['GET'])
 def api_listAll():
-    conn = sqlite3.connect('entries.db')
-    #conn.row_factory = dict_factory
+    conn = sqlite3.connect('notes.db')
+    conn.row_factory = dict_factory
     cur = conn.cursor()
-    all_books = cur.execute('SELECT * FROM tblNotes;').fetchall()
+    allNotes = cur.execute('SELECT * FROM Notes;').fetchall()
 
-    return jsonify(all_books)    
+    return jsonify(allNotes)    
 
 #Update or Delete
-@app.route('/api/v1/resources/entries/update', methods=['PUT', 'DELETE'])
+@app.route('/api/v1/resources/notes/update', methods=['PUT', 'DELETE'])
 def api_update():
+    conn = sqlite3.connect('notes.db')
+    c = conn.cursor()
+    #sql = "SELECT
     if request.method == 'PUT':
-        #TODO
+        id = request.args.get('ID')
         return null
     if request.method == 'DELETE':
         #TODO
         return null
         
-#Error Handler
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404 Error</h1><p>Sorry, there's been an error</p>", 404
- 
 app.run()
---
+++ frontend/src/components/Notes.vue
@@ -0,0 +1,238 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>Notes</h1>
+        <hr><br><br>
+        <alert :message=message v-if="showMessage"></alert>
+        <button type="button" class="btn btn-success btn-sm" v-b-modal.note-modal>Add Note</button>
+        <br><br>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Note</th>
+              <th scope="col">User</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(note, index) in notes" :key="index"> //console log confirms data is received, but data isnt showing in html table
+              <td>{{ note.Note }}</td>
+              <td>{{ note.User }}</td>
+              <td>
+                <div class="btn-group" role="group">
+                  <button
+                          type="button"
+                          class="btn btn-warning btn-sm"
+                          v-b-modal.note-update-modal
+                          @click="editNote(note)">
+                      Update
+                  </button>
+                  <button
+                          type="button"
+                          class="btn btn-danger btn-sm"
+                          @click="onDeleteNote(note)">
+                      Delete
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <b-modal ref="addNoteModal"
+            id="note-modal"
+            note="Add a new note"
+            hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+      <b-form-group id="form-note-group"
+                    label="Note:"
+                    label-for="form-note-input">
+          <b-form-input id="form-note-input"
+                        type="text"
+                        v-model="addNoteForm.note"
+                        required
+                        placeholder="Enter note">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-user-group"
+                      label="User:"
+                      label-for="form-user-input">
+            <b-form-input id="form-user-input"
+                          type="text"
+                          v-model="addNoteForm.user"
+                          required
+                          placeholder="Enter user">
+            </b-form-input>
+          </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
+    <b-modal ref="editNoteModal"
+            id="note-update-modal"
+            note="Update"
+            hide-footer>
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+      <b-form-group id="form-note-edit-group"
+                    label="Note:"
+                    label-for="form-note-edit-input">
+          <b-form-input id="form-note-edit-input"
+                        type="text"
+                        v-model="editForm.note"
+                        required
+                        placeholder="Enter note">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-user-edit-group"
+                      label="User:"
+                      label-for="form-user-edit-input">
+            <b-form-input id="form-user-edit-input"
+                          type="text"
+                          v-model="editForm.user"
+                          required
+                          placeholder="Enter user">
+            </b-form-input>
+          </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Update</b-button>
+          <b-button type="reset" variant="danger">Cancel</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Alert from './Alert.vue';
+
+export default {
+  data() {
+    return {
+      notes: [],
+      addNoteForm: {
+        note: '',
+        user: '',
+      },
+      message: '',
+      showMessage: false,
+      editForm: {
+        id: '',
+        note: '',
+        user: '',
+      },
+    };
+  },
+  components: {
+    alert: Alert,
+  },
+  methods: {
+    getNotes() {
+      const path = 'http://localhost:5000/api/v1/resources/notes/';
+      axios.get(path)
+		/*.then(function (response) {
+			console.log(response);
+		  })*/
+        .then((response) => {
+          this.notes = response.data.notes;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+    addNote(payload) {
+      const path = 'http://localhost:5000/api/v1/resources/notes/';
+      axios.post(path, payload)
+        .then(() => {
+          this.getNotes();
+          this.message = 'Note added!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getNotes();
+        });
+    },
+    initForm() {
+      this.addNoteForm.note = '';
+      this.addNoteForm.user = '';
+      this.editForm.id = '';
+      this.editForm.note = '';
+      this.editForm.user = '';
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addNoteModal.hide();
+      const payload = {
+        note: this.addNoteForm.note,
+        user: this.addNoteForm.user,
+      };
+      this.addNote(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addNoteModal.hide();
+      this.initForm();
+    },
+    editNote(note) {
+      this.editForm = note;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editNoteModal.hide();
+      const payload = {
+        note: this.editForm.note,
+        user: this.editForm.user,
+      };
+      this.updateNote(payload, this.editForm.id);
+    },
+    updateNote(payload, noteID) {
+      const path = `http://localhost:5000/api/v1/resources/notes/${noteID}`;
+      axios.put(path, payload)
+        .then(() => {
+          this.getNotes();
+          this.message = 'Note updated!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getNotes();
+        });
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editNoteModal.hide();
+      this.initForm();
+      this.getNotes();
+    },
+    removeNote(noteID) {
+      const path = `http://localhost:5000//api/v1/resources/notes/${noteID}`;
+      axios.delete(path)
+        .then(() => {
+          this.getNotes();
+          this.message = 'Note removed!';
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getNotes();
+        });
+    },
+    onDeleteNote(note) {
+      this.removeNote(note.id);
+    },
+  },
+  created() {
+    this.getNotes();
+  },
+};
+</script>
